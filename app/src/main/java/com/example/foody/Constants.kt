db4654f397cac43fb954299998737bb232ebda70
Wayne-Wang-Wayne <w702897028970289@gmail.com>
2023-07-23 20:27:38 +0800

    Create FoodRecipesApi interface and create related model classes.

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.foody
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "3b6f267e38984e89a3b34fe4fd8edddd"
+
+    }
+
+}
