c835157e7ed007e40e1b875dae7a302d256c70bc
Wayne-Wang-Wayne <w702897028970289@gmail.com>
2023-07-30 22:11:49 +0800

    Display api data in recyclerview

--- app/src/main/java/com/example/foody/util/Constants.kt
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -7,6 +7,15 @@ class Constants {
         const val BASE_URL = "https://api.spoonacular.com"
         const val API_KEY = "3b6f267e38984e89a3b34fe4fd8edddd"
 
+        // API Queries Key
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+
+
     }
 
 }
---
+++ app/src/main/java/com/example/foody/viewmodels/RecipesViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.foody.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.foody.util.Constants
+
+class RecipesViewModel(application: Application) : AndroidViewModel(application) {
+
+    fun applyQueries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+        queries[Constants.QUERY_NUMBER] = "50"
+        queries[Constants.QUERY_API_KEY] = Constants.API_KEY
+        queries[Constants.QUERY_TYPE] = "snack"
+        queries[Constants.QUERY_DIET] = "vegan"
+        queries[Constants.QUERY_ADD_RECIPE_INFORMATION] = "true"
+        queries[Constants.QUERY_FILL_INGREDIENTS] = "true"
+        return queries
+    }
+
+}
